9ebe0c8406c3571cf6b7edaee3e645ad45533760
ducnhat <nhattd.bk@gmail.com>
2018-06-11 10:03:31 +0700

    ConfigFCM_iOS

--- chat_app_react_native/ios/chat_app_react_native/AppDelegate.h
+++ chat_app_react_native/ios/chat_app_react_native/AppDelegate.h
@@ -7,7 +7,9 @@
 
 #import <UIKit/UIKit.h>
 
-@interface AppDelegate : UIResponder <UIApplicationDelegate>
+@import UserNotifications;
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate, UNUserNotificationCenterDelegate>
 
 @property (nonatomic, strong) UIWindow *window;
 
